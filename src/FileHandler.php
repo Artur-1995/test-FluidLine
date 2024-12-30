44e8bac89b1f4b26b29e71c5834bdd43b457ae44
Artur-1995 <89254423508@mail.ru>
2024-12-30 15:00:58 +0300

    Добавление комментариев и файла README.md

--- src/FileHandler.php
+++ src/FileHandler.php
@@ -1,16 +1,40 @@
 <?php
 
+/**
+ * Обработчик файла
+ * 
+ * PHP version 7.4.33
+ * 
+ * @author Avetisyan Artur <89254423508@mail.ru>
+ */
+
 namespace Src;
 
 include "FileTrait.php";
 
 use Exception;
 
-// Класс для обработки файла
+/**
+ * Класс для обработки файла
+ * 
+ * @throws Exception $e ошибка обработки файла
+ */
 class FileHandler
 {
     use FileTrait;
-    public function processFile(string $filename, bool $encoding = true): void
+
+    /**
+     * Обработка включает в себя разбиение слов по первым буквам и 
+     * дальнейшем распределении слов по файлам с названием первой буквы, 
+     * в котором хранится два файла один
+     * со списком слов начинающихся на эту букву, а в другом количество
+     * вхождений этой буквы в свмисок слов из первого файла
+     * 
+     * @param string $filename название файла
+     * 
+     * @throws Exception $e ошибка обработки файла
+     */
+    public function handler(string $filename): void
     {
         try {
             if (isset($_FILES['file'])) {
@@ -20,13 +44,14 @@ class FileHandler
                 }
             }
 
-            if ($encoding) {
+            if (!$this->checkCyrillicInFile($filename)) {
                 $filename = $this->convertFile($filename);
             }
 
             if (file_exists($filename)) {
                 $handle = fopen($filename, "r");
             }
+
             if ($handle) {
                 $letters = [];
 
@@ -45,6 +70,7 @@ class FileHandler
                 }
                 fclose($handle);
                 $this->writeInFile($letters);
+                echo 'Обработка завершена' . PHP_EOL;
             }
         } catch (Exception $e) {
             Logger::log([__CLASS__, __FUNCTION__], $e);
--- src/FileTrait.php
+++ src/FileTrait.php
@@ -1,21 +1,44 @@
 <?php
 
+
+/**
+ * Трейт для обработки файлов
+ * 
+ * PHP version 7.4.33
+ * 
+ * @author Avetisyan Artur <89254423508@mail.ru>
+ */
+
 namespace Src;
 
 use Exception;
 
+/**
+ * Трейт с набором методов для реализации обработки текстового файла
+ * в котором нарушена кодировка
+ * 
+ * @throws Exception $e ошибка обработки файла
+ */
 trait FileTrait
 {
-    // Метод для записи данных в файлы
-    public function writeInFile($letters): void
+    /**
+     * Метод для записи данных в файлы
+     * 
+     * @param array $letters массив с файловой структурой и данными
+     * 
+     * @throws Exception $e ошибка обработки файла
+     * 
+     * @return void запись завершена
+     */
+    public function writeInFile(array $letters): void
     {
         try {
             foreach ($letters as $letter => $data) {
-                $dir = "library/$letter";
-                if (!is_dir($dir)) {
-                    mkdir($dir, 0777, true);
-                }
-                file_put_contents("$dir/words.txt", implode("\n", $data['words']));
+                $dir = $this->createFolder($letter);
+                file_put_contents(
+                    "$dir/words.txt",
+                    implode("\n", $data['words'])
+                );
                 file_put_contents("$dir/count.txt", $data['count']);
             }
         } catch (Exception $e) {
@@ -23,26 +46,62 @@ trait FileTrait
         }
     }
 
-    // Метод для создания файла с необходимой кодировкой
-    public function convertFile($filename, $from = 'windows-1252', $to = 'windows-1251'): ?string
+    /**
+     * Метод для создания папки
+     * 
+     * @param string $letter название папки
+     */
+    public function createFolder($letter): string
     {
+        $dir = "library/$letter";
+        if (!is_dir($dir)) {
+            mkdir($dir, 0777, true);
+        }
+
+        return $dir;
+    }
+
+    /**
+     * Метод для конвертации данных
+     * 
+     * Создает файл с преобразованием кодировки
+     * 
+     * @param string $filename название файла 
+     * @param string $to название корректной кодировки, 
+     * по умолчанию отсутствует
+     * 
+     * @throws Exception $e ошибка при чтении или записи
+     * 
+     * @param string $newFilename название нового файла
+     */
+    public function convertFile(
+        string $filename,
+        string $to = ''
+    ): ?string {
         try {
             $content = file_get_contents($filename);
-            $currentEncoding = mb_detect_encoding($content, mb_list_encodings(), true);
-            // Преобразуем строку из utf-8/latin1 в windows-1252
-            $newContent = iconv($currentEncoding, $from, $content);
-            // Преобразуем строку из однобайтной кодировки обратно в utf-8, выдав её за windows-1251
-            $newContent = iconv($to, $currentEncoding, $newContent);
-            $newFilename = 'russian_converted.txt';
-            file_put_contents($newFilename, $newContent);
 
+            $newContent = $this->textConvert($content, $to);
+
+            if ($this->checkCyrillic($newContent)) {
+                $newFilename = 'russian_converted.txt';
+                file_put_contents($newFilename, $newContent);
+            }
             return $newFilename;
         } catch (Exception $e) {
             Logger::log([__CLASS__, __FUNCTION__], $e);
         }
     }
 
-    // Метод копирует загруженый файл текущую директорию
+    /**
+     * Метод копирует загруженый файл в текущую директорию
+     * 
+     * @param string $filename название файла 
+     * 
+     * @throws Exception $e ошибка при чтении или записи
+     * 
+     * @param null|string $newFilename название нового файла или null
+     */
     public function copyUploadFile($filename): ?string
     {
         try {
@@ -55,10 +114,77 @@ trait FileTrait
 
             return $newfilename;
         } catch (Exception $e) {
-            Logger::log([__CLASS__, __FUNCTION__], 'wer');
             Logger::log([__CLASS__, __FUNCTION__], $e);
         }
 
         return null;
+    }
+
+    /**
+     * Метод проверят текст, является ли он кирилице или нет
+     * 
+     * @param string $content текст для проверки
+     * 
+     * @param bool результат проверки true/false
+     */
+    public function checkCyrillic(string $content): bool
+    {
+        return preg_match('/[\p{Cyrillic}]/u', $content);
+    }
+
+    /**
+     * Метод проверят текста из файла, является ли он кирилице или нет
+     * 
+     * @param string $filename название файла 
+     * 
+     * @throws Exception $e ошибка при чтении
+     * 
+     * @param bool $this->checkCyrillic результат работы функции true/false
+     */
+    public function checkCyrillicInFile(string $filename): bool
+    {
+        if (is_file($filename)) {
+            $content = file_get_contents($filename);
+        }
+
+        return $this->checkCyrillic($content);
+    }
+
+    /**
+     * Метод конвертации текста
+     * 
+     * Метод исправляет кодировку для правелного отображения кирилицы
+     * 
+     * @param string $content текст для проверки
+     * @param string $to название корректной кодировки
+     * по умолчанию отсутствует
+     * 
+     * @throws Exception $e ошибка при чтении
+     * 
+     * @param bool $this->checkCyrillic результат работы функции true/false
+     */
+    public function textConvert(string $content, $to = '')
+    {
+        $currentEncoding = mb_detect_encoding(
+            $content,
+            mb_list_encodings(),
+            true
+        );
+        $possibleEncodings = mb_list_encodings() + ['windows-1252'];
+        foreach ($possibleEncodings as $encoding) {
+            if (@iconv($currentEncoding, $encoding, $content)) {
+                $newContent = iconv($currentEncoding, $encoding, $content);
+                $newContent = iconv($to, $currentEncoding, $newContent);
+                if ($this->checkCyrillic($newContent)) {
+                    echo "Кодировка: $encoding \n";
+                    echo "Текст успешно сконвертирован \n";
+                    break;
+                } else {
+                    $this->textConvert($content, $to);
+                }
+            }
+        }
+
+        return $newContent;
     }
 }
